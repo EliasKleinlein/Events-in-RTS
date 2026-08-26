0514c04e7832f5067b4fd614aa1f7eb44e4e24ff
chr-d <275697199+chr-d@users.noreply.github.com>
2026-08-26 12:34:31 +0200

    add map markers for events

--- src/pages/EventMap.jsx
+++ src/pages/EventMap.jsx
@@ -1,7 +1,36 @@
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+import { useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
+import { getEvents } from "../api/fetch";
+
 const EventMap = () => {
+  const [events, setEvents] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const loadEvents = async () => {
+      try {
+        const data = await getEvents();
+        setEvents(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadEvents();
+  }, []);
+  if (isLoading) {
+    return <p className="text-center">Map is loading...</p>;
+  }
+
+  if (error) {
+    return <p className="text-center text-red-500">{error}</p>;
+  }
+
   const position = [52.500402, 13.446582];
   return (
     <div className="mx-auto w-full max-w-2xl py-8">
@@ -15,9 +44,25 @@ const EventMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>Hello World!</Popup>
-        </Marker>
+        {events.map((event) => {
+          if (event.latitude && event.longitude) {
+            return (
+              <Marker position={[event.latitude, event.longitude]}>
+                <Popup>
+                  <div className="py-2 font-bold">{event.title}</div>
+                  <div>
+                    {new Date(event.date).toLocaleString("de-DE", {
+                      dateStyle: "medium",
+                      timeStyle: "short",
+                    })}
+                  </div>
+                  <div>{event.location}</div>
+                  <div className="py-2">{event.description}</div>
+                </Popup>
+              </Marker>
+            );
+          }
+        })}
       </MapContainer>
     </div>
   );
