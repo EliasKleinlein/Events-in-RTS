3cc41e5d396ca842be7299da4b31fea0b98f72c6
chr-d <275697199+chr-d@users.noreply.github.com>
2026-08-27 14:35:22 +0200

    add mock buttons to event details

--- src/pages/EventsDetails.jsx
+++ src/pages/EventsDetails.jsx
@@ -41,28 +41,41 @@ const EventDetails = () => {
   const position = [event.latitude, event.longitude];
 
   return (
-    <article className="card bg-base-200 mx-auto my-8 max-w-2xl shadow-xl">
+    <article className="card bg-base-200 mx-4 my-8 max-w-2xl shadow-xl sm:mx-auto">
       <div className="card-body">
-        <h1 className="card-title text-3xl">{event.title}</h1>
-        {event.description && <p className="py-4">{event.description}</p>}
-        <p>
-          <DateIcon className="text-primary inline pr-2" />
-          {new Date(event.date).toLocaleString("de-DE", {
-            dateStyle: "medium",
-            timeStyle: "short",
-          })}
-        </p>
-        <p>
-          <LocationIcon className="text-primary inline pr-2" />
-          {event.location}
-        </p>
+        <div className="flex flex-col gap-6 sm:flex-row lg:items-stretch lg:justify-between">
+          <div className="flex-1">
+            <h1 className="card-title text-3xl">{event.title}</h1>
+            {event.description && <p className="py-4">{event.description}</p>}
+            <p>
+              <DateIcon className="text-primary inline pr-2" />
+              {new Date(event.date).toLocaleString("de-DE", {
+                dateStyle: "medium",
+                timeStyle: "short",
+              })}
+            </p>
+            <p>
+              <LocationIcon className="text-primary inline pr-2" />
+              {event.location}
+            </p>
+          </div>
+          <div className="flex flex-row items-end gap-3 sm:flex-col sm:justify-between">
+            <button className="btn btn-primary w-10">+</button>
+            <button className="btn btn-accent w-30">Buy Tickets</button>
+          </div>
+        </div>
+
         {position[0] && position[1] && (
           <div className="py-2">
             <MapContainer
               center={position}
               zoom={13}
               scrollWheelZoom={false}
-              style={{ height: "200px", width: "100%", background: "#1a1a1a" }}
+              style={{
+                height: "200px",
+                width: "100%",
+                background: "#1a1a1a",
+              }}
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
